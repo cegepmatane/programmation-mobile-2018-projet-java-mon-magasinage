536fb499100a7172758872df7a3f432a31dbb9a0
abeauquel <beauquel.alexandre@gmail.com>
2018-09-23 17:39:15 -0400

    Feat (Magasin) : Mise à jour du modele
    nouveaux champs

--- app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/modele/Magasin.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/modele/Magasin.java
@@ -7,12 +7,21 @@ public class Magasin {
     public static final String NOM_TABLE = "magasin";
     public static final String CHAMP_ID = "id_magasin";
     public static final String CHAMP_NOM = "nom";
+
     private int id;
     private String nom;
+    private String adresse;
+    private String ville;
+    private Double coorX;
+    private Double coorY;
 
-    public Magasin(int id, String nom) {
+    public Magasin(int id, String nom, String adresse, String ville, Double coorX, Double coorY) {
         this.id = id;
         this.nom = nom;
+        this.adresse = adresse;
+        this.ville = ville;
+        this.coorX = coorX;
+        this.coorY = coorY;
     }
 
     public Magasin() {
@@ -33,6 +42,39 @@ public class Magasin {
     public void setNom(String nom) {
         this.nom = nom;
     }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public Double getCoorX() {
+        return coorX;
+    }
+
+    public void setCoorX(Double coorX) {
+        this.coorX = coorX;
+    }
+
+    public Double getCoorY() {
+        return coorY;
+    }
+
+    public void setCoorY(Double coorY) {
+        this.coorY = coorY;
+    }
+
     public HashMap<String, String> obtenirObjetPourAdapteur() {
         HashMap<String, String> magasinPourAdapteur = new HashMap<String, String>();
         magasinPourAdapteur.put(CHAMP_ID, String.valueOf(this.id));
