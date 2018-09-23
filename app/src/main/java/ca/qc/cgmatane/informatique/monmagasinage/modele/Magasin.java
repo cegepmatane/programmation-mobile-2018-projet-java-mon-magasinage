f11abddd70db0ad640e187fb66b01bb305dab3a2
abeauquel <beauquel.alexandre@gmail.com>
2018-09-23 15:33:28 -0400

    Feat (Magasin) : Def du Modele

--- app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/modele/Magasin.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/modele/Magasin.java
@@ -1,4 +1,33 @@
 package ca.qc.cgmatane.informatique.monmagasinage.modele;
 
 public class Magasin {
+<<<<<<< Updated upstream
+=======
+    private int id;
+    private String nom;
+
+    public Magasin(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public Magasin() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+>>>>>>> Stashed changes
 }
---
+++ app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/modele/pluriel/Magasins.java
@@ -0,0 +1,10 @@
+package ca.qc.cgmatane.informatique.monmagasinage.modele.pluriel;
+
+import java.util.ArrayList;
+
+import ca.qc.cgmatane.informatique.monmagasinage.modele.Magasin;
+
+public class Magasins extends ArrayList<Magasin>{
+
+
+}
