f21f82ad740b6bbbcbd314e51f9a8e0d1840c7a0
Hugo BLANCHARD <zaturgo@gmail.com>
2018-09-23 16:30:42 -0400

    Feat(modifierMagasin, AjouterMagasin)Création des vues

--- app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/vue/VueAjouterMagasin.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/vue/VueAjouterMagasin.java
@@ -1,4 +1,14 @@
 package ca.qc.cgmatane.informatique.monmagasinage.vue;
 
-public class VueAjouterMagasin {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ca.qc.cgmatane.informatique.monmagasinage.R;
+
+public class VueAjouterMagasin extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_ajouter_magasin);
+    }
 }
