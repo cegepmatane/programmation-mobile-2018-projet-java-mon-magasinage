f17c9d0f3fff7d2a4f55987f78f814104267dc56
paul <jacobs.paul.pro@gmail.com>
2018-09-23 19:02:05 -0400

    Feat(AjoutCourse) :  ajout de la navigation entre ajouter courses et ajouter produits

--- app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/vue/VueAjouterProduitListeCourse.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/vue/VueAjouterProduitListeCourse.java
@@ -8,21 +8,17 @@ import android.widget.Button;
 
 import ca.qc.cgmatane.informatique.monmagasinage.R;
 
-public class VueAjouterCourse extends AppCompatActivity {
-
-    private final int AJOUTER_PRODUIT_ID_RETOUR=0;
+public class VueAjouterProduitListeCourse extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vue_ajouter_course);
-
-        Button actionNaviguerAjouterProduitCourse = (Button) findViewById(R.id.vue_ajouter_course_action_ajouter_produit);
+        setContentView(R.layout.vue_ajouter_produit_liste_course);
+        Button actionNaviguerAjouterProduitCourse = (Button) findViewById(R.id.vue_ajouter_produit_liste_course_action_enregistrer);
         actionNaviguerAjouterProduitCourse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentionNaviguerAjouterProduitCourse = new Intent(VueAjouterCourse.this, VueAjouterProduitListeCourse.class);
-                startActivityForResult(intentionNaviguerAjouterProduitCourse, AJOUTER_PRODUIT_ID_RETOUR);
+                finish();
             }
         });
     }
