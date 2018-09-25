699e1ba1c82ac3a9e07cc5c6ef4e9802b8ad7b58
paul <jacobs.paul.pro@gmail.com>
2018-09-25 01:14:38 -0400

    Feat (modifierCourse): ajout de vueModifierCourse

---
+++ app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/vue/VueModifierCourse.java
@@ -0,0 +1,28 @@
+package ca.qc.cgmatane.informatique.monmagasinage.vue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.qc.cgmatane.informatique.monmagasinage.R;
+import ca.qc.cgmatane.informatique.monmagasinage.donnees.CourseDAO;
+import ca.qc.cgmatane.informatique.monmagasinage.modele.Course;
+
+public class VueModifierCourse extends AppCompatActivity {
+
+    private CourseDAO courseDAO = CourseDAO.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vue_modifier_course);
+
+        Bundle parametres = this.getIntent().getExtras();
+        String parametreIdCourse = (String) parametres.get("idCourse");
+        int idCourse = Integer.parseInt(parametreIdCourse);
+        
+
+
+    }
+
+
+}
