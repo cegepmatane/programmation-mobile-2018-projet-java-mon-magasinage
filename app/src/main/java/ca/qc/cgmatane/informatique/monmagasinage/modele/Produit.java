e0806996a3e90da2ca999dba90708d6ef07ea90c
abeauquel <beauquel.alexandre@gmail.com>
2018-09-25 10:45:10 -0400

    Feat : Modele produits

---
+++ app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/modele/Produit.java
@@ -0,0 +1,64 @@
+package ca.qc.cgmatane.informatique.monmagasinage.modele;
+
+public class Produit {
+    public static final String CHAMP_ID = "id_produit";
+    public static final String CHAMP_NOM = "nom";
+    public static final String CHAMP_QUANTITE_DEFAUT = "quantiteDefaut";
+    public static final String CHAMP_RECURENCE_ACHAT = "recurenceAchat";
+    public static final String CHAMP_ID_UNITE= "id_unite";
+
+    private int id;
+    private String nom;
+    private int quantiteDefaut;
+    private int recurenceAchat; //Jour
+    private Unite monUnite;
+
+    public Produit(int id, String nom, int quantiteDefaut) {
+        this.id = id;
+        this.nom = nom;
+        this.quantiteDefaut = quantiteDefaut;
+    }
+
+    public Produit() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getQuantiteDefaut() {
+        return quantiteDefaut;
+    }
+
+    public void setQuantiteDefaut(int quantiteDefaut) {
+        this.quantiteDefaut = quantiteDefaut;
+    }
+
+    public int getRecurenceAchat() {
+        return recurenceAchat;
+    }
+
+    public void setRecurenceAchat(int recurenceAchat) {
+        this.recurenceAchat = recurenceAchat;
+    }
+
+    public Unite getMonUnite() {
+        return monUnite;
+    }
+
+    public void setMonUnite(Unite monUnite) {
+        this.monUnite = monUnite;
+    }
+}
