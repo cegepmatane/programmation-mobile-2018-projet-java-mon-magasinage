1a0b23c9c9c37f6c7581ba51303ddf9f97fbeeaf
abeauquel <beauquel.alexandre@gmail.com>
2018-09-25 10:54:57 -0400

    Feat : Modele LigneCourse et LignesCourses

---
+++ app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/modele/LigneCourse.java
@@ -0,0 +1,55 @@
+package ca.qc.cgmatane.informatique.monmagasinage.modele;
+
+public class LigneCourse {
+    public static final String CHAMP_ID_COURSE ="id_course";
+    public static final String CHAMP_ID_PRODUIT ="id_produit";
+    public static final String CHAMP_QUANTITE ="quantite";
+    public static final String CHAMP_COCHE ="coche";
+
+    private Course course;
+    private Produit produit;
+    private int quantite;
+    private boolean coche;
+
+    public LigneCourse(Course course, Produit produit, int quantite, boolean coche) {
+        this.course = course;
+        this.produit = produit;
+        this.quantite = quantite;
+        this.coche = coche;
+    }
+
+    public LigneCourse() {
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Produit getProduit() {
+        return produit;
+    }
+
+    public void setProduit(Produit produit) {
+        this.produit = produit;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    public boolean isCoche() {
+        return coche;
+    }
+
+    public void setCoche(boolean coche) {
+        this.coche = coche;
+    }
+}
