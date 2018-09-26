c123bc9dc42e3059e6ac466c267b3fa1603b95d6
abeauquel <beauquel.alexandre@gmail.com>
2018-09-26 08:31:11 -0400

    Feat : Lister les magasins

--- app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/donnees/MagasinDAO.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/donnees/MagasinDAO.java
@@ -1,13 +1,24 @@
 package ca.qc.cgmatane.informatique.monmagasinage.donnees;
 
+import android.database.Cursor;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import ca.qc.cgmatane.informatique.monmagasinage.donnees.base.BaseDeDonnees;
+import ca.qc.cgmatane.informatique.monmagasinage.modele.Magasin;
+import ca.qc.cgmatane.informatique.monmagasinage.modele.pluriel.Magasins;
+
 public class MagasinDAO {
     private static MagasinDAO instance;
+    private BaseDeDonnees accesseurBaseDeDonnees;
+
+    protected Magasins listeMagasins;
 
     private MagasinDAO() {
+        listeMagasins = new Magasins();
+        accesseurBaseDeDonnees = BaseDeDonnees.getInstance();
     }
 
 
@@ -16,5 +27,39 @@ public class MagasinDAO {
             instance = new MagasinDAO();
         }
         return instance;
+    }
+
+    public Magasins listerMagasins(){
+        listeMagasins.clear();
+        Cursor curseurMagasin = accesseurBaseDeDonnees.getReadableDatabase().rawQuery("select * from "+ Magasin.NOM_TABLE, null);
+        Magasin magasin;
+
+        int indexId = curseurMagasin.getColumnIndex(Magasin.CHAMP_ID);
+        int indexNom = curseurMagasin.getColumnIndex(Magasin.CHAMP_NOM);
+        int indexAdresse = curseurMagasin.getColumnIndex(Magasin.CHAMP_ADRESSE);
+        int indexVille = curseurMagasin.getColumnIndex(Magasin.CHAMP_VILLE);
+        int indexCoorX = curseurMagasin.getColumnIndex(Magasin.CHAMP_COOR_X);
+        int indexCooY = curseurMagasin.getColumnIndex(Magasin.CHAMP_COOR_Y);
+
+        for(curseurMagasin.moveToFirst();!curseurMagasin.isAfterLast();curseurMagasin.moveToNext()){
+            int id = curseurMagasin.getInt(indexId);
+            String nom = curseurMagasin.getString(indexNom);
+            String ville = curseurMagasin.getString(indexVille);
+            String adresse = curseurMagasin.getString(indexAdresse);
+            Double coorX = curseurMagasin.getDouble(indexCoorX);
+            Double coorY = curseurMagasin.getDouble(indexCooY);
+
+            magasin= new Magasin(id, nom, adresse, ville, coorX, coorY);
+        }
+        curseurMagasin.close();
+        return listeMagasins;
+    }
+
+    public Magasins getListeMagasins() {
+        return listeMagasins;
+    }
+
+    public void setListeMagasins(Magasins listeMagasins) {
+        this.listeMagasins = listeMagasins;
     }
 }
