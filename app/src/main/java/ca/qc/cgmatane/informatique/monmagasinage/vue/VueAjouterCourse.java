41875206855b42eefe28cd917c519045ab1d4268
paul <jacobs.paul.pro@gmail.com>
2018-09-30 12:38:26 -0400

    Feat (AjouterCourse) : ajout de la selection d'une date

--- app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/vue/VueAjouterCourse.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/vue/VueAjouterCourse.java
@@ -1,18 +1,26 @@
 package ca.qc.cgmatane.informatique.monmagasinage.vue;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import java.io.Serializable;
 import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import ca.qc.cgmatane.informatique.monmagasinage.R;
 import ca.qc.cgmatane.informatique.monmagasinage.donnees.CourseDAO;
@@ -27,6 +35,9 @@ public class VueAjouterCourse extends AppCompatActivity {
 
     private CourseDAO courseDAO = CourseDAO.getInstance();
     private MagasinDAO magasinDAO = MagasinDAO.getInstance();
+    private final Calendar myCalendar = Calendar.getInstance(TimeZone.getDefault());
+    private EditText dateNotification;
+    Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +46,7 @@ public class VueAjouterCourse extends AppCompatActivity {
         setContentView(R.layout.vue_ajouter_course);
 
         final EditText nomCourse = findViewById(R.id.vue_ajouter_course_nom_course);
+        dateNotification = findViewById(R.id.vue_ajouter_course_date_notification);
         Spinner spinnerMagasin = findViewById(R.id.vue_ajouter_course_spinner_produit);
         Button actionNaviguerAjouterProduitCourse = (Button) findViewById(R.id.vue_ajouter_course_action_ajouter_produit);
 
@@ -62,9 +74,65 @@ public class VueAjouterCourse extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // TODO ajouter selection date
-                courseDAO.creerCourse(nomCourse.getText().toString(),"2000-01-01-00:00","2000-01-01-01:01","1",0);
+                courseDAO.creerCourse(nomCourse.getText().toString(),
+                        dateNotification.getText().toString(),
+                        "2000-01-01-01:01",
+                        "1",
+                        0);
                 finish();
             }
         });
+
+        dateNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new DatePickerDialog(VueAjouterCourse.this, date,
+                        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    final TimePickerDialog.OnTimeSetListener time = new TimePickerDialog.OnTimeSetListener() {
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            myCalendar.set(Calendar.HOUR, hourOfDay);
+            myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            myCalendar.set(Calendar.MINUTE, minute);
+            updateLabel();
+        }
+
+    };
+
+
+    final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+            TimePickerDialog mTimePicker;
+            mTimePicker = new TimePickerDialog(VueAjouterCourse.this, time, calendar.get(Calendar.HOUR),
+                    calendar.get(Calendar.MINUTE), true);//Yes 24 hour time
+            mTimePicker.setTitle("Select Time");
+            mTimePicker.show();
+
+        }
+
+    };
+
+
+
+    private void updateLabel(){
+        String myFormat = "yyyy-MM-dd-HH:mm"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+
+        dateNotification.setText(sdf.format(myCalendar.getTime()));
     }
 }
