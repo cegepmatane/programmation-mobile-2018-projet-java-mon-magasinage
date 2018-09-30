8d99e6046c5c4d09ed53d6b61b18208949fffe2d
paul <jacobs.paul.pro@gmail.com>
2018-09-30 12:36:17 -0400

    Feat (ModifierCourse): ajout de la vue modifier course

--- app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/vue/VueModifierCourse.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/vue/VueModifierCourse.java
@@ -1,7 +1,17 @@
 package ca.qc.cgmatane.informatique.monmagasinage.vue;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import ca.qc.cgmatane.informatique.monmagasinage.R;
 import ca.qc.cgmatane.informatique.monmagasinage.donnees.CourseDAO;
@@ -16,18 +26,20 @@ public class VueModifierCourse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         EnumerationTheme.changerTheme(this);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vue_modifier_course);
+        setContentView(R.layout.vue_modifier_course);
 
         Bundle parametres = this.getIntent().getExtras();
         String parametreIdCourse = (String) parametres.get(Course.CHAMP_ID_COURSE);
         int idCourse = Integer.parseInt(parametreIdCourse);
 
-        System.out.println(idCourse);
-
         Course courseAModifier = courseDAO.recupererCourseAvecId(idCourse);
+
+        EditText nomCourse = findViewById(R.id.vue_modifier_course_nom_course);
+        nomCourse.setText(courseAModifier.getNom());
+
+        System.out.println(courseAModifier.getMonMagasin().getId());
 
 
     }
-
 
 }
