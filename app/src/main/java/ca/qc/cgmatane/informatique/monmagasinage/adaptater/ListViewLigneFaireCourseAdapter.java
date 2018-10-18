14d13ad95c19beb51914d774e20922f576c45218
Hugo BLANCHARD <zaturgo@gmail.com>
2018-10-18 17:39:25 -0400

    erreurs

--- app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/adaptater/ListViewLigneFaireCourseAdapter.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/adaptater/ListViewLigneFaireCourseAdapter.java
@@ -10,57 +10,97 @@ import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import ca.qc.cgmatane.informatique.monmagasinage.R;
-import ca.qc.cgmatane.informatique.monmagasinage.donnees.UniteDAO;
 import ca.qc.cgmatane.informatique.monmagasinage.modele.Course;
 import ca.qc.cgmatane.informatique.monmagasinage.modele.LigneCourse;
-import ca.qc.cgmatane.informatique.monmagasinage.modele.pluriel.LignesCourse;
 import ca.qc.cgmatane.informatique.monmagasinage.modele.pluriel.Unites;
 
 public class ListViewLigneFaireCourseAdapter extends ArrayAdapter<LigneCourse> {
-    private LignesCourse listeCourse;
     private Course course;
     private Unites listeUnites;
     private Course courseActuelle;
     private Context monContext;
     private ArrayAdapter<String> adaptaterQuantite;
-    private int lastPosition = -1;
-    private static class VueBloque {
+    private static class VueBloqueFaireCourse {
         TextView textViewNomProduit;
         TextView textViewQuantite;
         TextView textViewUnite;
         CheckBox checkBoxLigneProduit;
     }
-    public ListViewLigneFaireCourseAdapter(LignesCourse listeLignes,Course course,  Activity context) {
+    public ListViewLigneFaireCourseAdapter(Course course,  Activity context) {
         super(context, R.layout.ligne_listview_faire_course);
-        this.listeCourse = listeCourse;
         this.monContext = context;
         courseActuelle = course;
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
     }
+    @Override
+    public LigneCourse getItem(int position) {
+        return course.getMesLignesCourse().get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    private int lastPosition = -1;
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LigneCourse ligneCourse = (LigneCourse) this.getItem(position);
-        VueBloque vueBloque;
+        System.out.println("ozifijoizejfoizjfoizjfoizejfzeoijfzeojfzio");
+        System.out.println("ozifijoizejfoizjfoizjfoizejfzeoijfzeojfzio");
+        System.out.println("ozifijoizejfoizjfoizjfoizejfzeoijfzeojfzio");
+        System.out.println("ozifijoizejfoizjfoizjfoizejfzeoijfzeojfzio");
+        System.out.println("ozifijoizejfoizjfoizjfoizejfzeoijfzeojfzio");
+        System.out.println("ozifijoizejfoizjfoizjfoizejfzeoijfzeojfzio");
+        System.out.println("ozifijoizejfoizjfoizjfoizejfzeoijfzeojfzio");
+        System.out.println("ozifijoizejfoizjfoizjfoizejfzeoijfzeojfzio");
+        VueBloqueFaireCourse vueBloqueFaireCourse;
         final View result;
         if (convertView == null) {
             //convertView = layoutInflater.inflate(R.layout.ligne_listview_produit, null);
-            vueBloque = new VueBloque();
+            vueBloqueFaireCourse = new VueBloqueFaireCourse();
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.ligne_listview_produit, parent, false);
+            convertView = inflater.inflate(R.layout.ligne_listview_faire_course, parent, false);
 
-            vueBloque.textViewNomProduit = convertView.findViewById(R.id.ligne_listview_faire_course_nom_produit);
-            vueBloque.textViewQuantite =  convertView.findViewById(R.id.ligne_listview_faire_course_quantite);
-            vueBloque.textViewUnite = convertView.findViewById(R.id.ligne_listview_faire_course_unite);
-            vueBloque.checkBoxLigneProduit = convertView.findViewById(R.id.ligne_listview_faire_course_check_box);
+            vueBloqueFaireCourse.textViewNomProduit = convertView.findViewById(R.id.ligne_listview_faire_course_nom_produit);
+            vueBloqueFaireCourse.textViewQuantite =  convertView.findViewById(R.id.ligne_listview_faire_course_quantite);
+            vueBloqueFaireCourse.textViewUnite = convertView.findViewById(R.id.ligne_listview_faire_course_unite);
+            vueBloqueFaireCourse.checkBoxLigneProduit = convertView.findViewById(R.id.ligne_listview_faire_course_check_box);
 
 
             result=convertView;
-            convertView.setTag(vueBloque);
+            convertView.setTag(vueBloqueFaireCourse);
         }else {
-            vueBloque = (VueBloque) convertView.getTag();
+            vueBloqueFaireCourse = (VueBloqueFaireCourse) convertView.getTag();
             result=convertView;
         }
         Animation animation = AnimationUtils.loadAnimation(monContext, (position > lastPosition) ? R.anim.haut_vers_le_bas : R.anim.bas_vers_le_haut);
@@ -68,18 +108,18 @@ public class ListViewLigneFaireCourseAdapter extends ArrayAdapter<LigneCourse> {
         lastPosition = position;
 
 
-        vueBloque.textViewNomProduit.setText(ligneCourse.getProduit().getNom());
-        vueBloque.textViewUnite.setText(ligneCourse.getUnite().getLibelle());
-        vueBloque.textViewQuantite.setText(ligneCourse.getQuantite());
-        vueBloque.checkBoxLigneProduit.setSelected(ligneCourse.isCoche());
-        vueBloque.checkBoxLigneProduit.setOnClickListener(new View.OnClickListener() {
+        vueBloqueFaireCourse.textViewNomProduit.setText(ligneCourse.getProduit().getNom());
+        vueBloqueFaireCourse.textViewUnite.setText(ligneCourse.getUnite().getLibelle());
+        vueBloqueFaireCourse.textViewQuantite.setText(ligneCourse.getQuantite());
+     /*   vueBloqueFaireCourse.checkBoxLigneProduit.setSelected(ligneCourse.isCoche());
+        vueBloqueFaireCourse.checkBoxLigneProduit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast message = Toast.makeText(monContext, //display toast message
                         "Check", Toast.LENGTH_SHORT);
                 message.show();
             }
-        });
+        });*/
         return convertView;
     }
 }
--- app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/vue/VueFaireCourse.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/monmagasinage/vue/VueFaireCourse.java
@@ -2,6 +2,7 @@ package ca.qc.cgmatane.informatique.monmagasinage.vue;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 
 import ca.qc.cgmatane.informatique.monmagasinage.R;
@@ -9,6 +10,7 @@ import ca.qc.cgmatane.informatique.monmagasinage.adaptater.ListViewLigneCourseAd
 import ca.qc.cgmatane.informatique.monmagasinage.adaptater.ListViewLigneFaireCourseAdapter;
 import ca.qc.cgmatane.informatique.monmagasinage.adaptater.ListViewProduitAdaptater;
 import ca.qc.cgmatane.informatique.monmagasinage.donnees.CourseDAO;
+import ca.qc.cgmatane.informatique.monmagasinage.donnees.LigneCourseDAO;
 import ca.qc.cgmatane.informatique.monmagasinage.modele.Course;
 import ca.qc.cgmatane.informatique.monmagasinage.modele.enumeration.EnumerationTheme;
 import ca.qc.cgmatane.informatique.monmagasinage.modele.pluriel.Produits;
@@ -19,21 +21,22 @@ public class VueFaireCourse extends AppCompatActivity {
     protected ListView listView;
     protected Produits listeProduits;
     protected CourseDAO courseDAO;
+    protected LigneCourseDAO ligneCourseDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         EnumerationTheme.changerTheme(this);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_faire_course);
         Bundle parametres = this.getIntent().getExtras();
         String parametreIdCourse = (String) parametres.get(Course.CHAMP_ID_COURSE);
         int idCourse = Integer.parseInt(parametreIdCourse);
         courseDAO = CourseDAO.getInstance();
+        ligneCourseDAO = ligneCourseDAO.getInstance();
         courseActuelle = courseDAO.getListeCourses().trouverAvecId(idCourse);
-        listeProduits = new Produits();
+        ligneCourseDAO.chargerListeLigneCoursePourUneCourse(courseActuelle);
         listView =(ListView) findViewById(R.id.list_view_faire_course);
-
-        listViewLigneFaireCourseAdapter = new ListViewLigneFaireCourseAdapter(courseActuelle.getMesLignesCourse(),courseActuelle, this);
+        listViewLigneFaireCourseAdapter = new ListViewLigneFaireCourseAdapter(courseActuelle, VueFaireCourse.this);
         listView.setAdapter(listViewLigneFaireCourseAdapter);
     }
 }
